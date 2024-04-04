3ec2549dfdcd19859c17108551f556a55a339609
worksapce <289582304@qq.com>
2024-04-04 20:16:45 +0800

    feat: module.py

---
+++ scripts/days/day2/module.py
@@ -0,0 +1,4 @@
+from ..demo.calculation import add, subtract, multiply, divide 
+
+ans = add(a = 1, b = 1)
+print("result: ", ans)
---
+++ scripts/days/demo/calculation.py
@@ -0,0 +1,13 @@
+test_data = 0
+
+def add(a: int, b: int) -> int:
+  return a + b
+
+def subtract(a: int, b: int) -> int:
+  return a - b
+
+def multiply(a: int, b: int) -> int:
+  return a * b
+
+def divide(a: int, b: int) -> int:
+  return a / b
