3bf1b0d9585139d280b671f4537f475e86d063aa
worksapce <289582304@qq.com>
2024-06-02 21:33:43 +0800

    feat: leetcdo 709

---
+++ scripts/leetcode/709-转化成小写字母.py
@@ -0,0 +1,10 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        s.lower()
+        return s.lower()
+
+
+
+# ------------------- TEST -------------------
+s = Solution()
+print(s.toLowerCase("HEllo"))
